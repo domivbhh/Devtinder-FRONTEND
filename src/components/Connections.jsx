2727fc26bb9c38801b6fad2aac10a5f419d0e670
domivbhh <hariharanvbit123@gmail.com>
2025-01-25 10:12:58 +0530

    connection page is created

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -3,6 +3,7 @@ import { backend } from '../utils'
 import UserCards from './UserCards'
 import { useDispatch, useSelector } from 'react-redux'
 import { addConnections } from '../store/slice/connectionSlice'
+import axios from 'axios'
 
 const Connections = () => {
 
@@ -17,18 +18,11 @@ const Connections = () => {
 
   const fetchConnections=async ()=>{
       try {
-        const  res=await fetch(`${backend}/user/myconnections`,{
-                    method:'GET',
-                    headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    token:localStorage.getItem('token')
-                    },
+        const  res=await axios(`${backend}/user/connections`,{
                     withCredentials: true,
                 })
-        const result=await res.json()
-        if(result.data){
-          dispatch(addConnections(result.data))
+        if(res.data){
+          dispatch(addConnections(res.data.data))
         }
       } 
 
@@ -37,24 +31,24 @@ const Connections = () => {
       }
   }
 
-  if(connections.length<0){
+  if(connections.length<1){
     return <div className='text-center font-bold text-xl'>
       <h1>No connections Found</h1>
     </div>
   }
 
   return (
-    <div className='text-center'>
-      <h1 className='text-3xl text-center text-gray-500 font-bold'>Connections</h1>
+    <div className='text-center p-4'>
+      <h1 className='text-2xl text-center text-gray-500 font-bold'>Connections</h1>
       {connections.length>0 && connections.map((ele)=>{
-        return <div className=' mx-auto flex gap-3 m-5 p-5 border rounded-lg bg-base-300 w-3/4'>
+        return <div className=' mx-auto flex gap-3 m-5 p-5 border rounded-lg bg-base-300 w-1/2'>
                     <div className='w-1/4'>
-                    <img src={ele?.photoUrl} className='w-20 h-20 rounded-full' alt="" />
+                    <img src={ele?.photoUrl} className='w-20 h-20 rounded-full' alt="image" />
                     </div>
                     <div className='text-left mx-4'>
-                    <h2 className='font-bold text-4xl'>{ele?.firstName+" "+ele?.lastName}</h2>
+                    <h2 className='font-bold text-xl'>{ele?.firstName+" "+ele?.lastName}</h2>
                     <p>{ele?.about}</p>
-                    <p>{ele?.age}</p>
+                    {/* <p>{ele?.age}</p> */}
                     </div>
 
               </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,50 +23,7 @@ const Navbar = () => {
   const{user}=useSelector((state)=>state.user)
   return (
     <div>
-      {/* <div className="navbar bg-base-200">
-        <div className="flex-1">
-          <Link className="btn btn-ghost text-xl" to={'/'}>DevTinder</Link>
-        </div>
-        {user?.firstName &&
-        <div className="flex-none gap-2">
-          <p>Welcome {user?.firstName}</p>
-        
-          <div className="dropdown dropdown-end mx-5 flex">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src={user && user?.photoUrl}
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <Link className="justify-between" to={'/profile'}>
-                  Profile
-                </Link>
-              </li>
-              <li>
-                <Link to={'/connections'}>Connections</Link>
-              </li>
-              <li>
-                <Link to={'/requests'}>Requests</Link>
-              </li>
-              <li onClick={()=>{localStorage.removeItem('token'),dispatch(removeUser()),navigate('/login'),window.location.reload()}}>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        }
-      </div> */}
+      
 
       <div className="navbar bg-base-100">
         <div className="flex-1">
@@ -99,7 +56,7 @@ const Navbar = () => {
                   </Link>
                 </li>
                 <li>
-                  <a>Settings</a>
+                  <Link to={'/connections'}>Connections</Link>
                 </li>
                 <li>
                   {
